08b92fb32decaa0e08b5c9320d66bbf1e4d9daeb
Matheus Henrique <matheusdeveloper.henrique@gmail.com>
2025-04-25 21:44:24 -0300

    feat: adding host 0.0.0.0 to work on render deploy

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app';
 import { env } from './env';
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
     console.log('🚀http server running');
 });
 
